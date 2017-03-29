e2f391d2dd18d70c87af964e69317712a118b817
miguelpalomino <miguel.manzano.palomino@gmail.com>
2017-03-29 19:28:04 +0100

    star 2 way binding

--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,13 +1,19 @@
 import {Component} from 'angular2/core';
 import {CoursesComponent} from './courses.component'
 import {AuthorsComponent} from './authors.component'
+import {StartComponent} from './start.component'
 
 @Component({
     selector: 'my-app',
     template: `
-    		<h1>My First Angular 2 App</h1>
-    		<courses></courses>
-    		<authors></authors>`,
-    directives: [CoursesComponent, AuthorsComponent]
+        <start></start>
+        <h1> {{title}}</h1>
+        <input type="text" [(ngModel)]="title" />
+        <courses></courses>
+        <authors></authors>
+        `,
+    directives: [CoursesComponent, AuthorsComponent, StartComponent]
 })
-export class AppComponent { }
+export class AppComponent {
+  title = " Angular App"
+}
---
+++ angular2-seed/app/start.component.ts
@@ -0,0 +1,20 @@
+import  {Component} from 'angular2/core'
+
+@Component({
+  selector: 'start',
+  template: `
+  <span
+    class="glyphicon"
+    [class.glyphicon-star]="isSelected"
+    [class.glyphicon-star-empty]="!isSelected"
+    (click)="onClick()"
+    aria-hidden="true">
+  </span>`,
+})
+export class StartComponent {
+  isSelected = false
+
+  onclick() {
+    this.isSelected = !this.isSelected
+  }
+}
